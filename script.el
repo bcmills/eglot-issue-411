345b0a26394fb6195ac157586e2d1ee6ec761a4f
Bryan C. Mills <bcmills@google.com>
2021-01-29 10:50:03 -0500

    script.el: update to use eglot-code-action-organize-imports
    Per https://github.com/joaotavora/eglot/issues/574#issuecomment-769645691.

--- script.el
+++ script.el
@@ -22,36 +22,14 @@
 (require 'yasnippet)
 (yas-global-mode 1)
 
-(defun eglot-organize-imports ()
-	"Offer to execute the source.organizeImports code action."
-	(interactive)
-	(unless (eglot--server-capable :codeActionProvider)
-		(eglot--error "Server can't execute code actions!"))
-	(let* ((server (eglot--current-server-or-lose))
-	       (actions (jsonrpc-request
-	                 server
-	                 :textDocument/codeAction
-	                 (list :textDocument (eglot--TextDocumentIdentifier))))
-	       (action (cl-find-if
-	                (jsonrpc-lambda (&key kind &allow-other-keys)
-	                  (string-equal kind "source.organizeImports" ))
-	                actions)))
-		(when action
-			(eglot--dcase action
-				(((Command) command arguments)
-					(eglot-execute-command server (intern command) arguments))
-				(((CodeAction) edit command)
-					(when edit (eglot--apply-workspace-edit edit))
-					(when command
-						(eglot--dbind ((Command) command arguments) command
-							(eglot-execute-command server (intern command) arguments))))))))
-
 (require 'go-mode)
 (with-eval-after-load "eglot"
 	(progn
 		(add-to-list 'eglot-server-programs '(go-mode . ("gopls" "-rpc.trace" "-logfile=/usr/local/google/home/bcmills/tmp/gopls.log")))
+		(defun eglot-interactively-organize-imports ()
+			(call-interactively 'eglot-code-action-organize-imports))
 		(defun eglot-install-save-hook ()
-			(add-hook 'before-save-hook #'eglot-organize-imports -20 t)
+			(add-hook 'before-save-hook #'eglot-interactively-organize-imports -20 t)
 			(add-hook 'before-save-hook #'eglot-format-buffer -10 t))
 		(add-hook 'go-mode-hook #'eglot-install-save-hook)
 		(add-hook 'go-mode-hook #'eglot-ensure)))
